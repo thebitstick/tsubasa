f47b449aa30598c70fdd57bba03f6ebeed83f666
TheBitStick <the@bitstick.rip>
2020-07-19 10:11:39 -0500

    macOS Support

--- tsubasa.fish
+++ tsubasa.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 # Tsubasa
-# Script for sharing screenshots and/or status updates using [McKael/madonctl](https://github.com/McKael/madonctl)
+# Script for sharing screenshots and/or status updates using [ihabunek/toot](https://github.com/ihabunek/toot)
 
 set SWAY_ENABLED 0
 set basename (basename (status -f))
@@ -9,6 +9,7 @@ set arg
 set message
 set verbosity 0
 set fullname
+set uname (uname)
 
 function check_verbosity
 	if test $verbosity -eq 1
@@ -42,11 +43,15 @@ if ! command -q zenity
 	exit 1
 end
 
-if ! command -q notify-send
+if ! command -q notify-send && test "$uname" != "Darwin"
 	echo "$basename: notify-send not found in PATH. optional, but needed for notifications"
 end
 
-if ! command -q xdg-user-dir
+if ! command -q terminal-notifier && test "$uname" == "Darwin"
+	echo "$basename: terminal-notifier not found in PATH. optional, but needed for notifications"
+end
+
+if ! command -q xdg-user-dir && test "$uname" != "Darwin"
 	echo "$basename: freedesktop non-compliance detected"
 	exit 2
 end
@@ -66,7 +71,16 @@ for item in $argv
 end
 
 function screenshot
-	set location (xdg-user-dir PICTURES)
+	if test "$uname" != "Darwin"
+		set location (xdg-user-dir PICTURES)
+	else
+		if test "$DARWIN_PHOTOS" = ""
+			echo "$basename: please set DARWIN_PHOTOS variable to desired save directory. example (/Volumes/Photos)"
+			exit 3
+		else
+			set location $DARWIN_PHOTOS
+		end
+	end
 	set sublocation "Tsubasa/"(date '+%Y-%m')
 	set filename "screenshot-"(date '+%Y-%m-%d-%H-%M-%S')".png"
 	set fullname "$location/Screenshots/$sublocation/$filename"
@@ -74,60 +88,83 @@ function screenshot
 	if test ! -d $location
 		check_verbosity "mkdir -p '$location'"
 		mkdir -p "$location"
-		echo "$basename: xdg pictures directory defined but doesn't exist. creating $location"
+		echo "$basename: pictures directory defined but doesn't exist. creating $location"
 	end
 
 	if test ! -d $location/Screenshots/$sublocation
 		check_verbosity "mkdir -p '$location/Screenshots/$sublocation'"
 		mkdir -p "$location/Screenshots/$sublocation"
-		echo "$basename: tsubasa directory under xdg pictures directory doesn't exist. creating $location/Screenshots/$sublocation"
+		echo "$basename: tsubasa directory under pictures directory doesn't exist. creating $location/Screenshots/$sublocation"
 	end
 
-	if test "$DISPLAY" = ""
+	if test "$DISPLAY" = "" && test "$uname" != "Darwin"
 		echo "$basename: no X or Wayland display detected"
 		exit 1
 	end
 
-	# GNOME and Sway are the only desktops supported, sorry
-	if test "$XDG_SESSION_DESKTOP" = "sway"
-		set SWAY_ENABLED 1
-		if ! command -q grimshot
-			echo "$basename: grimshot not found in PATH. needed for screenshots"
-			exit 1
-		end
-	else if ! command -q gnome-screenshot
-		echo "$basename: gnome-screenshot not found in PATH. needed for screenshots"
+	if ! command -q screencapture && test "$uname" = "Darwin"
+		echo "$basename: screencapture not found in PATH. needed for screenshots"
 		exit 1
 	end
 
-	if test "$arg" = "" && test "$SWAY_ENABLED" -eq 0
-		check_verbosity "gnome-screenshot --file='$fullname'"
-		gnome-screenshot --file="$fullname"
-	else if test "$SWAY_ENABLED" -eq 0
-		check_verbosity "gnome-screenshot --$arg --file='$fullname'"
-		gnome-screenshot --"$arg" --file="$fullname"
-	else if test "$arg" = "" && test "$SWAY_ENABLED" -eq 1
-		check_verbosity "grimshot save screen '$fullname'"
-		grimshot save screen "$fullname"
-	else if test "$SWAY_ENABLED" -eq 1
-		if test "$arg" = "window"
-			check_verbosity "grimshot save win '$fullname'"
-			grimshot save win "$fullname"
+	# GNOME/Sway are the only supported Linux desktops.
+	# macOS is also supported.
+	if test "$uname" != "Darwin"
+		if test "$XDG_SESSION_DESKTOP" = "sway"
+			set SWAY_ENABLED 1
+			if ! command -q grimshot
+				echo "$basename: grimshot not found in PATH. needed for screenshots"
+				exit 1
+			end
+		else if ! command -q gnome-screenshot
+			echo "$basename: gnome-screenshot not found in PATH. needed for screenshots"
+			exit 1
+		end
+
+		if test "$arg" = "" && test "$SWAY_ENABLED" -eq 0
+			check_verbosity "gnome-screenshot --file='$fullname'"
+			gnome-screenshot --file="$fullname"
+		else if test "$SWAY_ENABLED" -eq 0
+			check_verbosity "gnome-screenshot --$arg --file='$fullname'"
+			gnome-screenshot --"$arg" --file="$fullname"
+		else if test "$arg" = "" && test "$SWAY_ENABLED" -eq 1
+			check_verbosity "grimshot save screen '$fullname'"
+			grimshot save screen "$fullname"
+		else if test "$SWAY_ENABLED" -eq 1
+			if test "$arg" = "window"
+				check_verbosity "grimshot save win '$fullname'"
+				grimshot save win "$fullname"
+			else
+				check_verbosity "grimshot save area '$fullname'"
+				grimshot save area "$fullname"
+			end
+		end
+	else if test "$uname" = "Darwin"
+		if test "$arg" = ""
+			check_verbosity "screencapture '$fullname'"
+			screencapture "$fullname"
 		else
-			check_verbosity "grimshot save area '$fullname'"
-			grimshot save area "$fullname"
+			check_verbosity "screencapture -i '$fullname'"
+			screencapture -i "$fullname"
 		end
 	end
-
+	
 	if test ! -f $fullname
 		echo "$basename: file not saved"
 		exit 1
 	else
-		if command -q notify-send
+		if command -q notify-send && test "$uname" != "Darwin"
 			check_verbosity "notify-send --icon='$fullname' --expire-time=5000 \
 				'$basename: Screenshot Saved' 'Saved to $fullname'"
 			notify-send --icon="$fullname" --expire-time=5000 \
 				"$basename: Screenshot Saved" "Saved to $fullname"
+		else if command -q terminal-notifier && test "$uname" = "Darwin"
+			check_verbosity "terminal-notifier -title 'Tsubasa' -contentImage '$fullname' \
+				-subtitle '$basename: Screenshot Saved' -message 'Saved to $fullname' \
+				-group '$basename' -open '$fullname'"
+			terminal-notifier -title "Tsubasa" -contentImage "$fullname" \
+				-subtitle "$basename: Screenshot Saved" \
+				-message "Saved to $fullname" -group "$basename" -open "file://$fullname"
 		end
 	end
 end
@@ -142,10 +179,17 @@ else if test "$arg" != "text"
 	exit 1
 end
 
-check_verbosity "message=(zenity --title='Share to Fediverse' --ok-label=Send \
-	--cancel-label=Cancel --text-info --editable --width=350 --height=250)"
-zenity --title='Share to Fediverse' --ok-label=Send --cancel-label=Cancel \
-	--text-info --editable --width=350 --height=250 | read -z message
+if test "$uname" != "Darwin"
+	check_verbosity "message=(zenity --title='Share to Fediverse' --ok-label=Send \
+		--cancel-label=Cancel --text-info --editable --width=350 --height=250)"
+	zenity --title='Share to Fediverse' --ok-label=Send --cancel-label=Cancel \
+		--text-info --editable --width=350 --height=250 | read -z message
+else if test "$uname" = "Darwin"
+	check_verbosity "message=(env GTK_THEME=Adwaita:dark zenity --title='Share to Fediverse' --ok-label=Send \
+		--cancel-label=Cancel --text-info --editable --width=350 --height=250)"
+	env GTK_THEME=Adwaita:dark zenity --title='Share to Fediverse' --ok-label=Send --cancel-label=Cancel \
+		--text-info --editable --width=350 --height=250 | read -z message
+end
 
 if test $pipestatus[1] -eq 1
 	echo "$basename: cancelled by user"
@@ -162,17 +206,26 @@ end
 
 if test $status -eq 1
 	echo "$basename: unable to post status"
-	if command -q notify-send
+	if command -q notify-send && test "$uname" != "Darwin"
 		check_verbosity "notify-send --expire-time=5000 '$basename' 'Unable to post status'"
 		notify-send --expire-time=5000 "$basename" "Unable to post status"
+	else if command -q terminal-notifier && test "$uname" = "Darwin"
+		check_verbosity "terminal-notifier -title 'Tsubasa' \
+			-message 'Unable to post status' -group '$basename'"
+		terminal-notifier -title "Tsubasa" -message "Unable to post status" -group "$basename"
 	end
 	exit 1
 else
 	echo "$basename: status post successful"
-	if command -q notify-send
+	if command -q notify-send && test "$uname" != "Darwin"
 		check_verbosity "notify-send --expire-time=5000 '$basename' 'Status Post Successful'"
 		notify-send --expire-time=5000 "$basename" "Status Post Successful"
+	else if command -q terminal-notifier && test "$uname" = "Darwin"
+		check_verbosity "terminal-notifier -title 'Tsubasa' \
+			-message 'Status Post Successful' -group '$basename'"
+		terminal-notifier -title "Tsubasa" -message "Status Post Successful" -group "$basename"
 	end
 end
 
 exit 0
+
