ae56c9e46fdc5993d7402f289f5d90cff571a27f
TheBitStick <the@bitstick.rip>
2020-07-28 21:02:41 -0500

    Made even more repetitive boolean logic more readable.

--- tsubasa.fish
+++ tsubasa.fish
@@ -48,9 +48,17 @@ function check_if_not_mac
 end
 
 function check_if_mac
-	if ! command -q $argv[1] && test "$uname" == "Darwin"
+	if ! command -q $argv[1] && test "$uname" = "Darwin"
 		echo "$basename: $argv[1] not found in PATH. $argv[2..-1]"
 		exit 1
+	end
+end
+
+function test_if_mac
+	if test "$uname" != "Darwin"
+		return 1
+	else if test "$uname" = "Darwin"
+		return 0
 	end
 end
 
@@ -75,7 +83,7 @@ for item in $argv
 end
 
 function screenshot
-	if test "$uname" != "Darwin"
+	if ! test_if_mac
 		set location (xdg-user-dir PICTURES)
 	else
 		set location $HOME/Pictures # Finder localizes the "display name" of some folders, so the path stays the same
@@ -96,28 +104,21 @@ function screenshot
 		echo "$basename: tsubasa directory under pictures directory doesn't exist. creating $location/Screenshots/$sublocation"
 	end
 
-	if test "$DISPLAY" = "" && test "$uname" != "Darwin"
+	if test "$DISPLAY" = "" && ! test_if_mac
 		echo "$basename: no X or Wayland display detected"
 		exit 1
 	end
-
-	if ! command -q screencapture && test "$uname" = "Darwin"
-		echo "$basename: screencapture not found in PATH. needed for screenshots"
-		exit 1
-	end
+	
+	check_if_mac screencapture needed for screenshots
 
 	# GNOME/Sway are the only supported Linux desktops.
 	# macOS is also supported.
-	if test "$uname" != "Darwin"
+	if ! test_if_mac
 		if test "$XDG_SESSION_DESKTOP" = "sway"
 			set SWAY_ENABLED 1
-			if ! command -q grimshot
-				echo "$basename: grimshot not found in PATH. needed for screenshots"
-				exit 1
-			end
-		else if ! command -q gnome-screenshot
-			echo "$basename: gnome-screenshot not found in PATH. needed for screenshots"
-			exit 1
+			check grimshot needed for screenshots
+		else
+			check gnome-screenshot needed for screenshots
 		end
 
 		if test "$arg" = "" && test "$SWAY_ENABLED" -eq 0
@@ -138,7 +139,7 @@ function screenshot
 				grimshot save area "$fullname"
 			end
 		end
-	else if test "$uname" = "Darwin"
+	else if ! test_if_mac
 		if test "$arg" = ""
 			check_verbosity "screencapture '$fullname'"
 			screencapture "$fullname"
@@ -155,12 +156,12 @@ function screenshot
 		echo "$basename: file not saved"
 		exit 1
 	else
-		if command -q notify-send && test "$uname" != "Darwin"
+		if command -q notify-send && ! test_if_mac
 			check_verbosity "notify-send --icon='$fullname' --expire-time=5000 \
 				'$basename: Screenshot Saved' 'Saved to $fullname'"
 			notify-send --icon="$fullname" --expire-time=5000 \
 				"$basename: Screenshot Saved" "Saved to $fullname"
-		else if command -q terminal-notifier && test "$uname" = "Darwin"
+		else if command -q terminal-notifier && test_if_mac
 			check_verbosity "terminal-notifier -title 'Tsubasa' -contentImage '$fullname' \
 				-subtitle '$basename: Screenshot Saved' -message 'Saved to $fullname' \
 				-group '$basename' -open '$fullname'"
@@ -181,12 +182,12 @@ else if test "$arg" != "text"
 	exit 1
 end
 
-if test "$uname" != "Darwin"
+if ! test_if_mac
 	check_verbosity "message=(zenity --title='Share to Fediverse' --ok-label=Send \
 		--cancel-label=Cancel --text-info --editable --width=350 --height=250)"
 	zenity --title='Share to Fediverse' --ok-label=Send --cancel-label=Cancel \
 		--text-info --editable --width=350 --height=250 | read -z message
-else if test "$uname" = "Darwin"
+else if test_if_mac
 	check_verbosity "osascript -e 'display dialog \"What\'s Happening?\" default answer \"\" \
 		with title \"Share to Fediverse\"' | cut -d: -f3"
 	osascript -e 'display dialog "What\'s Happening?" default answer "" with title "Share to Fediverse"' \
@@ -208,10 +209,10 @@ end
 
 if test $status -eq 1
 	echo "$basename: unable to post status"
-	if command -q notify-send && test "$uname" != "Darwin"
+	if command -q notify-send && ! test_if_mac
 		check_verbosity "notify-send --expire-time=5000 '$basename' 'Unable to post status'"
 		notify-send --expire-time=5000 "$basename" "Unable to post status"
-	else if command -q terminal-notifier && test "$uname" = "Darwin"
+	else if command -q terminal-notifier && test_if_mac
 		check_verbosity "terminal-notifier -title 'Tsubasa' \
 			-message 'Unable to post status' -group '$basename'"
 		terminal-notifier -title "Tsubasa" -message "Unable to post status" -group "$basename"
@@ -219,10 +220,10 @@ if test $status -eq 1
 	exit 1
 else
 	echo "$basename: status post successful"
-	if command -q notify-send && test "$uname" != "Darwin"
+	if command -q notify-send && ! test_if_mac
 		check_verbosity "notify-send --expire-time=5000 '$basename' 'Status Post Successful'"
 		notify-send --expire-time=5000 "$basename" "Status Post Successful"
-	else if command -q terminal-notifier && test "$uname" = "Darwin"
+	else if command -q terminal-notifier && test_if_mac
 		check_verbosity "terminal-notifier -title 'Tsubasa' \
 			-message 'Status Post Successful' -group '$basename'"
 		terminal-notifier -title "Tsubasa" -message "Status Post Successful" -group "$basename"
